3062389fb55785ca938f6ce65a32f1a94d5ebb66
Deependra kumar <144874909+deependra0411@users.noreply.github.com>
2023-09-13 16:08:01 +0530

    Update MySQL using python for employee.py

--- MySQL using python for employee.py
+++ MySQL using python for employee.py
@@ -2,17 +2,17 @@ import mysql.connector
 
 class Employee:
     def __init__(self, host, user, password, database_name, table_name):
-        self.my_db = mysql.connector.connect(host=host, user=user, password=password)  # use to setup conncetion btwn sql and python
-        self.cur = self.my_db.cursor()               # intilizing cursor inorder to work in python
-        self.database_name = database_name          # adding database name as class attribute so that can be used by any method of the given class
-        self.table_name = table_name               # adding table name as class attribute so that can be used by any method of the given class
+        self.my_db = mysql.connector.connect(host=host, user=user, password=password)  
+        self.cur = self.my_db.cursor()              
+        self.database_name = database_name          
+        self.table_name = table_name              
 
     def create_database(self):
         self.cur.execute("CREATE DATABASE {}".format(self.database_name))
         self.my_db.commit()
 
     def create_table(self, column_names):
-        column_definitions = ', '.join('{} VARCHAR(255)'.format(col) for col in column_names)  # List comprehension for table column name creation
+        column_definitions = ', '.join('{} VARCHAR(255)'.format(col) for col in column_names)  
         create_table_query = "CREATE TABLE {} ({})".format(self.table_name, column_definitions)
         self.cur.execute("USE {}".format(self.database_name))
         self.cur.execute(create_table_query)
@@ -48,10 +48,10 @@ class Employee:
             self.cur.execute(query, values)
             self.my_db.commit()
         else:
-            print("Employee with ID {} not exists.".format(id))  # can use f string also
+            print("Employee with ID {} not exists.".format(id)) 
 
     def promote_employee(self, id, new_post, salary_increase_percentage):
-        query = "UPDATE {} SET post = %s, salary = salary + salary * %s / 100 WHERE id = %s".format(self.table_name)  # like in sql update Table_name Col1=x,Col2=y.... where id =Z
+        query = "UPDATE {} SET post = %s, salary = salary + salary * %s / 100 WHERE id = %s".format(self.table_name) 
         values = (new_post, salary_increase_percentage, id)
         self.cur.execute("USE {}".format(self.database_name))
         self.cur.execute(query, values)
@@ -60,22 +60,23 @@ class Employee:
     def close_connection(self):
         self.cur.close()
         self.my_db.close()
-database_name = "empd"
-table_name = "empllo"
-e1 = Employee("localhost", "root", "admin123", database_name, table_name)
+        
+database_name = "database_name"
+table_name = "table_name"
+e1 = Employee("host", "user", "password", database_name, table_name)
 
 
 e1.create_database()
-e1.create_table(("id", "name", "post", "salary"))
-e1.insert_employee(id=1, name="Ravi", post="senior manager", salary=90000)
-e1.insert_employee(id=2, name="Ravindar jadeja", post='manager', salary=70000)
-e1.insert_employee(id=3, name="senorita", post="receptionist", salary=30000)
-e1.insert_employee(id=4, name="renuka", post="team lead", salary=60000)
-e1.insert_employee(id=5, name="rajendra", post="president", salary=1205864)
-e1.delete_employee(6)
-##e1.promote_employee(2, "Vice president", 0)
 
-s = "SELECT * FROM {}".format(table_name)
+e1.create_table(("id", "name", "post", "salary"))
+e1.insert_employee(id=1, name="name", post="post", salary=salary)
+e1.insert_employee(id=2, name="name", post="post", salary=salary)
+e1.insert_employee(id=3, name="name", post="post", salary=salary)
+
+e1.delete_employee(1)          # For deleting employee using Id
+e1.promote_employee(2, "New_Post", salary incremenmt percentage)   # For Promoting employee usinf Id, New_Post, Percentage increase in salary.
+
+s = "SELECT * FROM {}".format(table_name)   # To fetch all Employees
 e1.cur.execute(s)
 result = e1.cur.fetchall()
 
