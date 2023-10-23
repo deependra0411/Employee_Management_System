30fa609dbe50b0fa419f5cb7bbddcca96b78ab13
Deependra kumar <144874909+deependra0411@users.noreply.github.com>
2023-10-23 18:40:36 +0530

    Update MySQL using python for employee.py

--- MySQL using python for employee.py
+++ MySQL using python for employee.py
@@ -1,86 +1,116 @@
 import mysql.connector
-
-class Employee:
-    def __init__(self, host, user, password, database_name, table_name):
-        self.my_db = mysql.connector.connect(host=host, user=user, password=password)  
-        self.cur = self.my_db.cursor()              
-        self.database_name = database_name          
-        self.table_name = table_name              
+from configuration.config import parameters
+class EmployeeDatabase:
+    def __init__(self, database_name):
+        self.connection = mysql.connector.connect(host=parameters["host"], user=parameters["user"],  password=parameters["password"])
+        self.cursor = self.connection.cursor()
+        self.database_name = database_name
 
     def create_database(self):
-        self.cur.execute("CREATE DATABASE {}".format(self.database_name))
-        self.my_db.commit()
-
-    def create_table(self, column_names):
-        column_definitions = ', '.join('{} VARCHAR(255)'.format(col) for col in column_names)  
-        create_table_query = "CREATE TABLE {} ({})".format(self.table_name, column_definitions)
-        self.cur.execute("USE {}".format(self.database_name))
-        self.cur.execute(create_table_query)
-        self.my_db.commit()
-
-    def check_duplicate(self, id):
-        query = "SELECT COUNT(*) FROM {} WHERE id = %s".format(self.table_name)
-        value = (id,)
-        self.cur.execute("USE {}".format(self.database_name))
-        self.cur.execute(query, value)
-        count = self.cur.fetchone()[0]
-        return count > 0
-
-    def insert_employee(self, **kwargs):
-        if self.check_duplicate(kwargs.get('id')):
-            print("Employee with ID {} already exists.".format(kwargs.get('id')))
-            return
-
-        column_names = ', '.join(kwargs.keys())
-        placeholders = ', '.join('%s' for _ in kwargs.values())
-        query = "INSERT INTO {} ({}) VALUES ({})".format(self.table_name, column_names, placeholders)
-        values = tuple(kwargs.values())
-        self.cur.execute("USE {}".format(self.database_name))
-        self.cur.execute(query, values)
-        self.my_db.commit()
-        print("Employee added successfully.")
-
-    def delete_employee(self,id):
-        if self.check_duplicate(id):
-            query = "DELETE FROM {} WHERE id = %s".format(self.table_name)
-            values = (id,)
-            self.cur.execute("USE {}".format(self.database_name))
-            self.cur.execute(query, values)
-            self.my_db.commit()
-        else:
-            print("Employee with ID {} not exists.".format(id)) 
-
-    def promote_employee(self, id, new_post, salary_increase_percentage):
-        query = "UPDATE {} SET post = %s, salary = salary + salary * %s / 100 WHERE id = %s".format(self.table_name) 
-        values = (new_post, salary_increase_percentage, id)
-        self.cur.execute("USE {}".format(self.database_name))
-        self.cur.execute(query, values)
-        self.my_db.commit()
+        try:
+            query = f"CREATE DATABASE {self.database_name}"
+            self.cursor.execute(query)
+            self.connection.commit()
+        except mysql.connector.Error as err:
+            print(f"Error creating the database: {err}")
 
     def close_connection(self):
-        self.cur.close()
-        self.my_db.close()
-        
-database_name = "database_name"
-table_name = "table_name"
-e1 = Employee("host", "user", "password", database_name, table_name)
+        self.cursor.close()
+        self.connection.close()
 
+class Employee:
+    def __init__(self, db, table_name):
+        self.db = db
+        self.table_name = table_name
 
-e1.create_database()
+    def create_table(self, column_names):
+        try:
+            column_definitions = ', '.join('{} VARCHAR(255)'.format(col) for col in column_names)
+            print("col definition",column_definitions)
+            query = "CREATE TABLE {} ({})".format(self.table_name, column_definitions)
+            self.db.cursor.execute("USE {}".format(self.db.database_name))
+            self.db.cursor.execute(query)
+            self.db.connection.commit()
+        except mysql.connector.Error as err:
+            print(f"Error creating the table: {err}")
 
-e1.create_table(("id", "name", "post", "salary"))
-e1.insert_employee(id=1, name="name", post="post", salary=salary)
-e1.insert_employee(id=2, name="name", post="post", salary=salary)
-e1.insert_employee(id=3, name="name", post="post", salary=salary)
+    def check_duplicate(self, id):
+        try:
+            query = "SELECT COUNT(*) FROM {} WHERE id = %s".format(self.table_name)
+            value = (id,)
+            self.db.cursor.execute("USE {}".format(self.db.database_name))
+            self.db.cursor.execute(query, value)
+            count = self.db.cursor.fetchone()[0]
+            return count > 0
+        except mysql.connector.Error as err:
+            print(f"Error checking for duplicates: {err}")
+            return False
 
-e1.delete_employee(1)          # For deleting employee using Id
-e1.promote_employee(2, "New_Post", salary incremenmt percentage)   # For Promoting employee using Id, New_Post, Percentage increase in salary.
+    def insert_employee(self, **kwargs):
+        try:
+            if self.check_duplicate(kwargs.get('id')):
+                print("Employee with ID {} already exists.".format(kwargs.get('id')))
+                return
+            column_names = ', '.join(kwargs.keys())
+            placeholders = ', '.join('%s' for _ in kwargs.values())
+            query = "INSERT INTO {} ({}) VALUES ({})".format(self.table_name, column_names, placeholders)
+            values = tuple(kwargs.values())
+            self.db.cursor.execute("USE {}".format(self.db.database_name))
+            self.db.cursor.execute(query, values)
+            self.db.connection.commit()
+            print("Employee added successfully.")
+        except mysql.connector.Error as err:
+            print(f"Error inserting employee: {err}")
 
-s = "SELECT * FROM {}".format(table_name)   # To fetch all Employees
-e1.cur.execute(s)
-result = e1.cur.fetchall()
+    def delete_employee(self, id):
+        try:
+            if self.check_duplicate(id):
+                query = "DELETE FROM {} WHERE id = %s".format(self.table_name)
+                values = (id,)
+                self.db.cursor.execute("USE {}".format(self.db.database_name))
+                self.db.cursor.execute(query, values)
+                self.db.connection.commit()
+            else:
+                print("Employee with ID {} does not exist.".format(id))
+        except mysql.connector.Error as err:
+            print(f"Error deleting employee: {err}")
 
-for rec in result:
-    print(rec)
+    def promote_employee(self, id, new_post, salary_increase_percentage):
+        try:
+            query = "UPDATE {} SET post = %s, salary = salary + salary * %s / 100 WHERE id = %s".format(self.table_name)
+            values = (new_post, salary_increase_percentage, id)
+            self.db.cursor.execute("USE {}".format(self.db.database_name))
+            self.db.cursor.execute(query, values)
+            self.db.connection.commit()
+            print("Employee promoted successfully.")
+        except mysql.connector.Error as err:
+            print(f"Error promoting employee: {err}")
 
-e1.close_connection()
+def main():
+    database_name = "Wipro"
+    table_name = "employees"
+    db = EmployeeDatabase(database_name)
+    e1 = Employee(db, table_name)
+
+    db.create_database()
+    e1.create_table(("id", "name", "post", "salary"))
+
+    e1.insert_employee(id=1, name="Ravi", post="senior manager", salary=90000)
+    e1.insert_employee(id=2, name="Ravindar jadeja", post="manager", salary=70000)
+    e1.insert_employee(id=3, name="senorita", post="receptionist", salary=30000)
+    e1.insert_employee(id=4, name="renuka", post="team lead", salary=60000)
+    e1.insert_employee(id=5, name="rajendra", post="president", salary=1205864)
+    e1.delete_employee(6)
+    e1.promote_employee(2, "Vice president", 10)
+
+    select_query = f"SELECT * FROM {table_name}"
+    e1.db.cursor.execute(select_query)
+    result = e1.db.cursor.fetchall()
+
+    for rec in result:
+        print(rec)
+
+    db.close_connection()
+
+if __name__ == "__main__":
+    main()
